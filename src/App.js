f6cab7a330fab17ccfb9010d2f2d6bbe4f4b2cfe
Jeremiah <jeremiah.d.wise@gmail.com>
2020-12-09 12:26:40 -0600

    Add google sign-in with firebase

--- src/App.js
+++ src/App.js
@@ -5,19 +5,42 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx';
 import Header from './components/header/header.jsx';
+import { auth } from './firebase/firebase/utils.js';
 
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-      </Switch>
-    </div>
-  );
+
+
+class App extends React.Component() {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user})
+
+      console.log(user);
+    });
+  }
+
+  render (){
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyAol1ENEZ8Y2aG8l41R6RvXufCHunEqZyM",
+    authDomain: "cwn-db-11dd3.firebaseapp.com",
+    projectId: "cwn-db-11dd3",
+    storageBucket: "cwn-db-11dd3.appspot.com",
+    messagingSenderId: "1054078234533",
+    appId: "1:1054078234533:web:4ee1ffad5d1852d026c24c",
+    measurementId: "G-L437N3EB8G"
+  };
+
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+// We want to always trigger the google popup whenever we use the GoogleAuthProvider
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
